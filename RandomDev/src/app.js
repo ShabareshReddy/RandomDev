840336665acaa8dd305312970f5c7510f0dd0a15
ShabareshReddy <chandrareddy85580@gmail.com>
2026-02-18 03:22:35 +0530

    changes in backend

--- RandomDev/src/app.js
+++ RandomDev/src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require("express");
 const {connectionDB} = require("./config/database.js");
 const app=express();
--- RandomDev/src/config/database.js
+++ RandomDev/src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectionDB=async()=>{
-    await mongoose.connect(
-        "mongodb+srv://milkabhau87:TzHQ9RuMGLHembNJ@namastenode.e5moccv.mongodb.net/divTinder"
-    );
+    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports={connectionDB};
