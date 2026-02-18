64932085f1bf7d1d0ded1b08d74948648a5fbe0e
Shabaresh Reddy <chandrareddy85580@gmail.com>
2026-02-18 12:11:08 +0530

    small changes
    Removed commented out JWT_SECRET variable.

--- RandomDev/src/models/user.js
+++ RandomDev/src/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-// const JWT_SECRET = "NamasteBhai@1234";
 
 const userSchema = new mongoose.Schema(
   {
